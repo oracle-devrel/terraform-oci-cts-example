5b1c6faafeb5a81ed348b37d03cbb476ec81e41f
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-09-17 08:45:51 -0600

    feat: initial commit

---
+++ variables.tf
@@ -0,0 +1,57 @@
+
+variable "services" {
+  description = "Consul services monitored by Consul-Terraform-Sync"
+  type = map(
+    object({
+      id        = string
+      name      = string
+      kind      = string
+      address   = string
+      port      = number
+      meta      = map(string)
+      tags      = list(string)
+      namespace = string
+      status    = string
+
+      node                  = string
+      node_id               = string
+      node_address          = string
+      node_datacenter       = string
+      node_tagged_addresses = map(string)
+      node_meta             = map(string)
+
+      cts_user_defined_meta = map(string)
+    })
+  )
+}
+
+# variable "catalog_services" {
+#   description = "Consul catalog service names and tags monitored by Consul Terraform Sync"
+#   type = map(list(string))
+# }
+
+variable "lb_id" {
+  type        = string
+  description = "The OCID of the OCI LB to use."
+}
+
+variable "be_set_name" {
+  type        = string
+  description = "The name of the LB Backend Set to use."
+}
+
+variable "region" {
+  type        = string
+  description = "The OCI region to use."
+}
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key" {
+  default = ""
+}
+variable "private_key_path" {
+  default = ""
+}
+variable "private_key_password" {}
+variable "fingerprint" {}
