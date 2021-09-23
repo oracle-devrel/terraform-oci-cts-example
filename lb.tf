0742df71cc0690d9293c77b7030bb28675ba1bb2
tim_clegg <10459168+timclegg@users.noreply.github.com>
2021-09-23 15:34:28 -0600

    feat: Adding the ability to filter by service status (make it more responsive)

--- lb.tf
+++ lb.tf
@@ -1,13 +1,13 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 
 resource "oci_load_balancer_backend" "this" {
-  for_each = var.services
+  for_each = local.cts_services
 
   backendset_name  = var.be_set_name
   ip_address       = each.value.address
   load_balancer_id = var.lb_id
   port             = each.value.port
-
+  
   backup  = false
   drain   = false
   offline = false
